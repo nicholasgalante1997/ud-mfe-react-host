8eab119c0a467f3813425e083108d09b2d43252b
Nick G <nicholasgalante1997@gmail.com>
2021-11-26 14:42:05 -0700

    release workflow

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const common = require('./webpack.common.js');
@@ -14,9 +13,6 @@ const dev = {
         }
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
         new ModuleFederationPlugin({
             name: 'host',
             filename: 'remoteEntry.js',
