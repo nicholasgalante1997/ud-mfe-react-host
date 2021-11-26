8df1abfd6451c93e7dd6005a0a0b43b7dd256186
Nick G <nicholasgalante1997@gmail.com>
2021-11-26 13:01:01 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+const App = () => (
+    <React.Fragment>
+        <h3>Micro Federatin'</h3>
+        <hr />
+        <MarketingApp />
+    </React.Fragment>
+);
+
+export default App;
